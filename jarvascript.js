e3c82eeafc633019e02d75ecc6b66500fdde486d
Jason Reid <jasonreid470@yahoo.com>
2023-02-17 13:18:28 +1000

    module 1

---
+++ jarvascript.js
@@ -0,0 +1,24 @@
+let images = ["dice.png",
+"dice 2.png",
+"dice 3.png",
+"dice 4.png",
+"dice 5.png",
+"dice 6.png"];
+let dice = document.querySelectorAll("images");
+
+function roll(){
+    dice.forEach(function(die){
+        die.classList.add("shake");    
+
+});
+
+setTimeout(function)(){
+    dice.forEach(function(die){
+        die.classList.remove("shake");
+
+    });
+
+});
+
+
+}
